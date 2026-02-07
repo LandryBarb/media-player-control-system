c25be0076654f449fbb87518c8d80c8ac4a62d9e
Landry Barb II <landrybarb@pm.me>
2026-02-07 08:14:28 -0800

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// js/main.js
+
+// Reserved for future progressive enhancement.
+// Example: theme toggler to add/remove `.theme-dark` on <html> or <body>.
+
+document.documentElement.classList.remove('no-js');
